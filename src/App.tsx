1f824428e60b5b42f8f36d4d5f1734e871498162
ri OH <leesulgi66@naver.com>
2024-10-17 19:00:03 +0900

    LoadingScreen

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import Login from "./routes/login";
 import CreateAccount from "./routes/create-account";
 import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
+import { useEffect, useState } from "react";
+import LoadingScreen from "./components/loading-screen";
 
 const router = createBrowserRouter([
   {
@@ -44,9 +46,18 @@ const GlobalStyles = createGlobalStyle`
 `;
 
 function App() {
+  const [isLoading, setLoading] = useState(true);
+  const init = async() => {
+    // wait for firebase
+    // setTimeout(()=> setIsLoading(false), 2000);  // loding test
+    setLoading(false);
+  }
+  useEffect(() => {
+    init();
+  }, [])
   return <>
     <GlobalStyles />
-    <RouterProvider router={router} />
+    {isLoading ? <LoadingScreen /> : <RouterProvider router={router} />}
   </>;
 }
 
