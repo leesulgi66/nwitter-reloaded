efc757229c354545b125e719f0b193e42ad14870
ri OH <leesulgi66@naver.com>
2024-10-29 15:57:43 +0900

    Tweeing to Firestore

--- src/components/post-tweet-form.tsx
+++ src/components/post-tweet-form.tsx
@@ -1,5 +1,7 @@
+import { addDoc, collection } from "firebase/firestore";
 import { useState } from "react";
 import styled from "styled-components"
+import { auth, db } from "../firebase";
 
 const Form = styled.form`
     display: flex;
@@ -69,8 +71,25 @@ export default function PostTweetForm() {
             setFile(files[0]);
         }
     }
-
-    return <Form>
+    const onSubmit = async(e:React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const user = auth.currentUser
+        if(!user || isLoading || tweet ==="" || tweet.length > 180) return;
+        try {
+            await addDoc(collection(db, "tweets"), {
+                tweet,
+                createAt:Date.now(),
+                username: user.displayName || "Anonymous",
+                userId: user.uid,
+            });
+            setLoading(true);
+        }catch(e) {
+            console.log(e);
+        }finally {
+            setLoading(false);
+        }
+    }
+    return <Form onSubmit={onSubmit}>
         <TextArea rows={5} maxLength={180} onChange={onChange} value={tweet} placeholder="What is happening?!"/>
         <AttachFileButton htmlFor="file">{file ? "Potho added✔" : "Add photo"}</AttachFileButton>
         <AttachFileInput onChange={onFileChange} type="file" id="file" accept="image/*"/>
