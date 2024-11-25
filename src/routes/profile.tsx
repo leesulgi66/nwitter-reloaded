7decd9a9a387f96cc61957a83a5a1faf4d60606c
ri OH <leesulgi66@naver.com>
2024-11-25 17:56:04 +0900

    user profile page css

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -1,3 +1,50 @@
+import styled from "styled-components";
+import { auth } from "../firebase"
+import { useState } from "react";
+
+const Wrapper = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    gap: 20px;
+`;
+const AvatarUpload = styled.label`
+    width: 80px;
+    overflow: hidden;
+    height: 80px;
+    border-radius: 50%;
+    background-color: #1d9bf0;
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    svg{
+        width: 50px;
+    }
+`;
+const AvatarImg = styled.img`
+    width: 100%;
+
+`;
+const AvatarInput = styled.input`
+    display: none;
+`;
+const Name = styled.span`
+    font-size: 22px;
+`;
 export default function Profile() {
-    return <h1>Profile</h1>
+    const user = auth.currentUser;
+    const [avatar, setAvatar] = useState(user?.photoURL);
+    return (<Wrapper>
+        <AvatarUpload htmlFor="avatar">
+            {avatar ? <AvatarImg src={avatar} /> : <svg data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"></path>
+            </svg>}
+        </AvatarUpload>
+        <AvatarInput id="avatar" type="file" accept="image/*" />
+        <Name>
+            {user?.displayName ?? "Anonymous"}
+        </Name>
+    </Wrapper>
+    );
 }
