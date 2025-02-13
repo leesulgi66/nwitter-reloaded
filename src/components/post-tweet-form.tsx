f4bd05c64da0fcc8cfab699f1787e46eec6fbc3e
ri OH <leesulgi66@naver.com>
2025-02-13 14:13:50 +0900

    change

--- src/components/post-tweet-form.tsx
+++ src/components/post-tweet-form.tsx
@@ -1,8 +1,8 @@
 import { addDoc, collection, updateDoc } from "firebase/firestore";
-import { useState } from "react";
+import React, { useState } from "react";
 import styled from "styled-components"
 import { auth, db } from "../firebase";
-import AWS from 'aws-sdk';
+import AWS from "aws-sdk";
 
 const Form = styled.form`
     display: flex;
@@ -19,9 +19,9 @@ const TextArea = styled.textarea`
     background-color: black;
     width: 100%;
     resize: none;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     &::placeholder {
         font-size: 16px;
-        font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     }
     &:focus {
         outline: none;
@@ -51,18 +51,18 @@ const SubmitBtn = styled.input`
     padding: 10px 0px;
     border-radius: 20px;
     font-size: 16px;
-    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     cursor: pointer;
     &:hover,
     &:active {
-        opacity: 0.85;
+        opacity: 0.8;
     }
 `;
 
-export default function PostTweetForm() {
+export default function PostTweetFrorm() {
+    //S3 setting--------------------------------------
     const ACCESS_KEY = import.meta.env.VITE_ACCESS_KEY
     const SECRET_KEY = import.meta.env.VITE_SECRET_KEY
-    const RESION = "ap-northeast-2";
+    const REGION = "ap-northeast-2";
     const S3_BUKKET = "sulgibucket";
     AWS.config.update({
         accessKeyId: ACCESS_KEY,
@@ -70,12 +70,13 @@ export default function PostTweetForm() {
     });
     const myBuket = new AWS.S3({
         params: { Buket: S3_BUKKET },
-        region: RESION,
+        region: REGION,
     });
+    //------------------------------------------------
     const [isLoading, setLoading] = useState(false);
     const [tweet, setTweet] = useState("");
-    const [file, setFile] = useState<File | null>(null);
-    const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const [file, setFile] = useState<File|null>(null);
+    const onChange = (e:React.ChangeEvent<HTMLTextAreaElement>) => {
         setTweet(e.target.value);
     }
     const onFileChange = (e: React.ChangeEvent<HTMLInputElement>)=> {
@@ -83,6 +84,7 @@ export default function PostTweetForm() {
         if(files && files.length === 1) {
             if(files[0].size < 1*1024*1024){
                 setFile(files[0]);
+                console.log("file on");
             }else {
                 alert("image file size should be less than 1Mb");
             }
@@ -91,25 +93,19 @@ export default function PostTweetForm() {
     const onSubmit = async(e:React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         const user = auth.currentUser
-        if(!user || isLoading || tweet ==="" || tweet.length > 180) return;
-        try {
+        if(!user || isLoading || tweet === "" || tweet.length > 180) return;
+        try{
             setLoading(true);
             const doc = await addDoc(collection(db, "tweets"), {
                 tweet,
-                createAt:Date.now(),
+                createdAt: Date.now(),
                 username: user.displayName || "Anonymous",
                 userId: user.uid,
-                photo: "",
+                photo : "", 
                 photoKey: "",
             });
-            if(file) { // file을 업로드 후 url주소 받기
-                // //firestore 방법
-                // const locationRef = ref(storage, `tweets/${user.uid}/${doc.id}`); //img file의 경로를 설정
-                // const result = await uploadBytes(locationRef, file); //promise를 반환하고, 그 결과값에 업로드 결과에 대한 참조가 있다.
-                // const url = await getDownloadURL(result.ref); //result 값을 참조해 url주소를 반환받는다.
-                // console.log(url);
-                // await updateDoc(doc, {photo: url}); // 위에서 추가한 doc에 photo항목을 추가해 준다.
 
+            if(file) {
                 //S3 upload
                 const params = {
                     ACL : "public-read",
@@ -118,29 +114,28 @@ export default function PostTweetForm() {
                     ContentType: file.type,
                     Key : "upload/" + file.name,
                 };
-                
-                await myBuket.upload(params, (error:any)=>{
+                const url = await myBuket.upload(params, (error:any)=>{
                     console.log(error);
-                }).promise().then((data)=>{
-                    const url = data.Location;
-                    const key = data.Key;
-                    console.log(url);
-                    updateDoc(doc, {photo: url, photoKey : key});
-                });
+                }).promise()
+                await updateDoc(doc, {
+                    photo: url.Location,
+                    photoKey : url.Key
+                })
             }
-            setTweet("");
-            setFile(null);
-            window.location.reload();
         }catch(e) {
             console.log(e);
         }finally {
             setLoading(false);
+            setTweet("");
+            setFile(null);
         }
     }
-    return <Form onSubmit={onSubmit}>
-        <TextArea rows={5} maxLength={180} onChange={onChange} value={tweet} placeholder="What is happening?" required/>
+    return (
+    <Form onSubmit={onSubmit}>
+        <TextArea required rows={5} maxLength={180} onChange={onChange} value={tweet} placeholder="What is happening?"/>
         <AttachFileButton htmlFor="file">{file ? "Photo added ✅" : "Add photo"}</AttachFileButton>
-        <AttachFileInput onChange={onFileChange} type="file" id="file" accept="image/*"/>
+        <AttachFileInput onChange={onFileChange} type="file" id="file" accept="image/*" />
         <SubmitBtn type="submit" value={isLoading ? "Posting..." : "Post Tweet"}/>
     </Form>
+    )
 }
